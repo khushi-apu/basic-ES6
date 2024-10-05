f4496abd591dbfd8dad3cadc195e1b25ca4ce290
khushi-apu <tabassumkhushi17@gmail.com>
2024-10-06 02:40:31 +0600

    map

--- map.js
+++ map.js
@@ -1,8 +1,4 @@
 const numbers = [4,9,85,63,12,4]
-function doubleIt(num)
-{
-    return num * 2;
-}
-// console.log(doubleIt(5));
-const result = numbers.map(doubleIt)
+
+const result = numbers.map(numbers=>numbers*2)
   console.log(result);
