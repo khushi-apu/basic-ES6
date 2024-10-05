8465cde815e912603da8f7b23b10b609ec8a0e09
khushi-apu <tabassumkhushi17@gmail.com>
2024-10-06 03:18:45 +0600

    filter.js

--- filter.js
+++ filter.js
@@ -1,4 +1,5 @@
 const numbers= [1,5,63,8,7]
 const players=[45,8,9,3,74]
-const result = players.filter(p=> p>70)
+// const result = players.filter(p=> p>70)
+const result = players.filter(p=> p%2===1)
 console.log(result)
