168eb7dd94cc997d680728baa45cc32c175d4ec5
khushi-apu <tabassumkhushi17@gmail.com>
2024-10-06 01:33:10 +0600

    object,array destructure

---
+++ chain.js
@@ -0,0 +1,14 @@
+// data access
+
+const data=[{id:1, name: 'akash',address:'bangladesh'}];
+// console.log(data[0].address)
+
+const product={
+    count:'5000',
+    data: [
+        { id:1, name:'lenevo laptop',price:'50000'},
+        { id:2, name:'Asus laptop',price:'120000'},
+        { id:3, name:'HP laptop',price:'150000'}
+    ]
+}
+console.log(product.data[2].price)
