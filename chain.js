5bb6a7ac4417f0b615ea1e5014ac60a9b812e6d2
khushi-apu <tabassumkhushi17@gmail.com>
2024-10-06 02:21:41 +0600

    array

--- chain.js
+++ chain.js
@@ -5,10 +5,14 @@ const data=[{id:1, name: 'akash',address:'bangladesh'}];
 
 const product={
     count:'5000',
+    name:{
+        client:'kobita'
+    }
     data: [
         { id:1, name:'lenevo laptop',price:'50000'},
         { id:2, name:'Asus laptop',price:'120000'},
         { id:3, name:'HP laptop',price:'150000'}
     ]
 }
-console.log(product.data[2].price)
+// console.log(product.data[2].price)
+console.log(product.name?.client)
---
+++ map.js
@@ -0,0 +1,6 @@
+const numbers = [4,9,85,63,12,4]
+function doubleIt(num)
+{
+    return num * 2;
+}
+console.log(doubleIt(5));
