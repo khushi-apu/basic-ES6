aefad7c95a99d9f3943c8b3615b7bdc87c5eb71b
khushi-apu <tabassumkhushi17@gmail.com>
2024-10-06 03:16:41 +0600

    filter.js

---
+++ filter.js
@@ -0,0 +1,4 @@
+const numbers= [1,5,63,8,7]
+const players=[45,8,9,3,74]
+const result = players.filter(p=> p>70)
+console.log(result)
---
+++ forEach.js
@@ -0,0 +1,7 @@
+const numbers= [1,5,63,8,7]
+// const result=numbers.forEach(n => console.log(n)
+// );
+const result= numbers.forEach(n=>{
+
+})
+    
